5ec5730001f62f83c1815267f2673a7a783c49aa
Ana Clara <151066171+anaclarall@users.noreply.github.com>
2024-08-06 18:20:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("sobre").addEventListener("click", function() {
+  window.location.href = "sobre.html";
+});
+document.getElementById("softskills").addEventListener("click", function() {
+  window.location.href = "softskills.html";
+});
+function voltarParaInicio() {
+  window.location.href = "/";
+}
